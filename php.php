de9173d3eb0e65da728f0aa409bac48c5a475360
Shreya549 <43725310+Shreya549@users.noreply.github.com>
2019-05-26 12:25:50 +0530

    Update php.php

--- php.php
+++ php.php
@@ -3,14 +3,21 @@ $servername = "localhost";
 $username = "root";
 $password = "1242";
 $db_name = "login";
-
 $conn = mysqli_connect($servername, $username, $password,$db_name);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$mail = $_POST["user"];
+$pass = $_POST["pass"];
+$sql = "SELECT * FROM loginid WHERE pass='cc1234' ";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["pass"]."<br>";
+    }
 }
-else{
- echo " Successfully linked";
-}
-
+else
+{
+    echo "No files found";}
 ?>
